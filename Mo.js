a70d3f637e67e7bc284b927ea65336dce57e7de9
Daniel Vitor Morilha <dmorilha@gmail.com>
2012-09-13 21:36:42 +0000

    changing several points, looking for the promised performance

--- Mo.js
+++ Mo.js
@@ -225,28 +225,21 @@
                     lines = [],
                     t = 0;
 
-                lines.push('var buffer = new Buffer(total);');
-                lines.push('var string;');
-                lines.push('var stringLenght;');
-                lines.push('var i = 0;');
+                lines.push('function (i) {');
+                lines.push(tab(1) + 'var lines = [];');
                 lines.push('');
 
                 moParser.on('data', function (o) {
                     var a = [],
                         b = [],
-                        i = 0,
-                        l,
-                        s;
+                        l;
 
                     switch (o.type) {
 
                     case 'initial':
                         if (o.data) {
-                            s = o.data.toString();
-                            l = s.length;
-                            t += l;
-                            lines.push('buffer.write(\'' + self.parseString(s) + '\', i);');
-                            lines.push('i += ' + l.toString() + ';');
+                            lines.push(tab(1) + 'lines.push(\'' + self.parseString(o.data.toString()) + '\');');
+                            lines.push('');
                         }
 
                         break;
@@ -254,24 +247,15 @@
                     case 'evaluation':
                         if (o.data) {
                             a = o.data.split('.');
-                            a.unshift('input');
+                            a.unshift('i');
 
                             for (i = 2, l = a.length; i <= l; i++) {
                                 b.push(a.slice(0, i).join('.'));
                             }
 
-                            lines.push('');
-                            lines.push('if (' + b.join(' && ') + ') {');
-                            lines.push(tab(1) + 'string = ' + b[b.length - 1] + '.toString();');
-                            lines.push(tab(1) + 'stringLength = string.length;');
-                            lines.push(tab(1) + 'if (stringLength > left) {');
-                            lines.push(tab(2) + 'left = resize(buffer, stringLength - left);');
-                            lines.push(tab(1) + '} else {');
-                            lines.push(tab(2) + 'left -= stringLength;');
+                            lines.push(tab(1) + 'if (' + b.join(' && ') + ') {');
+                            lines.push(tab(2) + 'l.push(' + b[l - 1] + '.toString());');
                             lines.push(tab(1) + '}');
-                            lines.push(tab(1) + 'buffer.write(string, i);');
-                            lines.push(tab(1) + 'i += stringLength;');
-                            lines.push('}');
                             lines.push('');
                         }
 
@@ -280,24 +264,8 @@
                 });
 
                 moParser.on('end', function () {
-                    var i = 1,
-                        x = t;
-
-                    while (x >>= 1) {
-                        i++;
-                    }
-
-                    x = 0x1 << i;
-
-                    while (x - t < 1024) {
-                        x <<= 1;
-                    }
-
-                    lines.unshift('var total = ' + x.toString() + ';'); // calculated;
-                    lines.unshift('var left = ' + (x - t).toString() + ';'); // left;
-
-                    lines.push('');
-                    lines.push('output(buffer.slice(0, i));');
+                    lines.push(tab(1) + 'output(buffer.slice(0, i));');
+                    lines.push('}');
 
                     cb({
                         code: lines.join('\n')
@@ -310,55 +278,26 @@
     };
 
     (new Mo).compile('./template', function (o) {
-        var i,
-            script;
+        var c,
+            i,
+            z;
 
-        function resize(buffer, size) {
-            var b,
-                r = buffer.length,
-                s = r + size,
-                i = 1,
-                x = t;
+        c = 'assignTemplate(' + o.code + ');';
 
-            while (x >>= 1) {
-                i++;
+        vm.runInNewContext(c, {
+            assignTemplate: function (f) {
+                z = f
             }
+        });
 
-            x = 0x1 << i;
-
-            while (x - s < 1024) {
-                x <<= 1;
-            }
-
-            b = new Buffer(x);
-            buffer.copy(b, r);
-        }
-
-        console.error(o.code);
-
-        script = require('vm').createScript(o.code)
-
-        for (i = 0; i < 1; i++) {
-            script.runInNewContext({
-                Buffer: Buffer,
-
-                //console: console,
-
-                input: {
-                    a: {
-                        a: 'foo',
-                        b: 'bar',
-                        c: 'baz'
-                    },
-                    b: 'foz'
+        for (i = 0; i < 1000; i++) {
+            console.log(({a: {
+                    a: 'foo',
+                    b: 'bar',
+                    c: 'baz'
                 },
-
-                output: function (b) {
-                    console.log(b.toString('utf-8'));
-                },
-
-                resize: resize
-            });
+                b: 'foz'
+            }));
         }
     });
 }());
